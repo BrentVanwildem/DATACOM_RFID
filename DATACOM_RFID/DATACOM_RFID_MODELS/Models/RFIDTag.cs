dff699d8282037af82bb35be06cca82f346f4d01
BrentVanwildem <brent.vanwildemeersch@hotmail.be>
2016-04-04 15:56:53 +0200

    Verschillende Tabbladen

---
+++ DATACOM_RFID/DATACOM_RFID_MODELS/Models/RFIDTag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATACOM_RFID_MODELS.Models
+{
+    public class RFIDTag
+    {
+        public int ID { get; set; }
+        public int RFID { get; set; }
+    }
+}
